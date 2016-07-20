28129b9b76bf1592ea3f3ab635e6f67dc1c02562
operation badar <ob@gmail.com>
2016-07-20 15:40:07 -0700

    tasks
    tasks

--- ch51.py
+++ ch51.py
@@ -3,6 +3,6 @@ age = 12
        uprice = 0
         elif age < 18:
            vprice = 5
-else:
-    wprice = 10
+               else:
+          wprice = 10
                          print("Your admission cost is $" + str(price) + ".")
---
+++ task6.py
@@ -0,0 +1,4 @@
+a = range(1,9)
+for list in a:
+ c = list**2
+ print(list,c)
